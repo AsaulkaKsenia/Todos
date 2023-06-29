76bcc28eef5352854e58da40160fb7b0821d01d3
Asaulka Ksenia <asa.ru.80@mail.ru>
2023-06-29 20:44:09 +0300

    Initial commit

---
+++ src/main/java/ru/netology/SimpleTask.java
@@ -0,0 +1,14 @@
+package ru.netology;
+
+public class SimpleTask extends Task{
+    private String title;
+
+    public SimpleTask(int id, String title) {
+        super(id);
+        this.title = title;
+
+
+        
+
+    }
+}
